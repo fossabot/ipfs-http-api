b2591907ff80c77cfa60f42bfcb2379d857fa7a8
Roy van de Water <roy.vwater@gmail.com>
2017-12-29 11:02:07 -0700

    v2.1.0 Add Cat method

---
+++ cat.go
@@ -0,0 +1,20 @@
+package ipfs
+
+import (
+	"io"
+	"net/url"
+
+	"github.com/computes/ipfs-http-api/http"
+)
+
+// Cat returns a reader for the data in IPFS located at address
+func Cat(ipfsURL url.URL, address string) (io.ReadCloser, error) {
+	query := url.Values{}
+	query.Add("arg", address)
+
+	catURL := ipfsURL
+	catURL.Path = "/api/v0/cat"
+	catURL.RawQuery = query.Encode()
+
+	return http.Get(catURL.String())
+}
