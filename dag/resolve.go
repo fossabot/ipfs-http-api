aeae112b52d5d260aac8ad044c9b2cfd572db8d0
Roy van de Water <roy.vwater@gmail.com>
2017-12-29 11:08:03 -0700

    v2.2.0 Add dag.Resolve

---
+++ dag/resolve.go
@@ -0,0 +1,42 @@
+package dag
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/url"
+
+	"github.com/computes/ipfs-http-api/http"
+)
+
+// Resolve resolves a ipld reference in IPFS
+func Resolve(ipfsURL url.URL, address string) (string, error) {
+	query := url.Values{}
+	query.Add("arg", address)
+
+	dagResolveURL := ipfsURL
+	dagResolveURL.Path = "/api/v0/dag/resolve"
+	dagResolveURL.RawQuery = query.Encode()
+
+	reader, err := http.Get(dagResolveURL.String())
+	if err != nil {
+		return "", err
+	}
+
+	data, err := ioutil.ReadAll(reader)
+	if err != nil {
+		return "", err
+	}
+
+	resolveResponse := struct {
+		Cid struct {
+			Address string `json:"/"`
+		}
+	}{}
+
+	err = json.Unmarshal(data, &resolveResponse)
+	if err != nil {
+		return "", err
+	}
+
+	return resolveResponse.Cid.Address, nil
+}
