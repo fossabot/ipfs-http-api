bdda05d4091c41e5bec204a878d276e5cd437ed2
Roy van de Water <roy.vwater@gmail.com>
2017-12-30 10:34:38 -0700

    v2.4.0 Add a pin.Add method

---
+++ pin/add.go
@@ -0,0 +1,20 @@
+package pin
+
+import (
+	"net/url"
+
+	"github.com/computes/ipfs-http-api/http"
+)
+
+// Add pins a an IPFS object recursively
+func Add(ipfsURL url.URL, address string) error {
+	query := url.Values{}
+	query.Add("arg", address)
+
+	pinAddURL := ipfsURL
+	pinAddURL.Path = "/api/v0/pin/add"
+	pinAddURL.RawQuery = query.Encode()
+
+	_, err := http.Get(pinAddURL.String())
+	return err
+}
